6fc8649a47245e423137ce549d7297959b9b7126
Clement <clement7903@hotmail.com>
2024-10-10 17:43:35 +0800

    test busAPI in production

--- src/api/busAPI.js
+++ src/api/busAPI.js
@@ -5,7 +5,7 @@ import axios from 'axios';
  */
 const apiKey = import.meta.env.VITE_API_KEY;
 
-const BUS_FORECAST_URL = 'https://cors-anywhere.herokuapp.com/https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival?BusStopCode=83139';
+const BUS_FORECAST_URL = `${import.meta.env.VITE_API_URL}/ltaodataservice/v3/BusArrival?BusStopCode=83139`;
 const BusForecastAPI = axios.create({
     baseURL: BUS_FORECAST_URL,
     headers: {
