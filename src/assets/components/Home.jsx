cb263c63cc6f89b2258d7ac7beb7ea58deb34c60
Az2366 <az.wong2366@gmail.com>
2024-10-17 19:32:42 +0800

    add UserBar component, edit AuthContectProvider. Need Login to access Navbar

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,28 +1,18 @@
-import { useContext } from 'react';
-import NavbarComponent from './NavbarComponent';
-import { AuthContext } from './AuthContextProvider';
+import { useContext } from "react";
+import NavbarComponent from "./NavbarComponent";
+import { AuthContextProvider } from "./AuthContextProvider";
+import UserBar from "./UserBar";
 
 function Home() {
-    const authCtx = useContext(AuthContext);
+  return (
+      <div>
+        <AuthContextProvider>
+        <NavbarComponent />
+        <UserBar />
+        </AuthContextProvider>
+      </div>
 
-    return (
-        <div>
-            <NavbarComponent />
-            <h2>Welcome home!!</h2>
-
-            {authCtx.isLoggedIn ? (
-                <>
-                    <p>You are logged in!</p>
-                    <button onClick={authCtx.logoutHandler} className="btn btn-danger">Logout</button>
-                </>
-            ) : (
-                <>
-                    <p>You are not logged in, click the login button to log in.</p>
-                    <button className="btn btn-primary" onClick={authCtx.loginHandler}>Login</button>
-                </>
-            )}
-        </div>
-    );
+  );
 }
 
 export default Home;
--- src/assets/components/NavbarComponent.jsx
+++ src/assets/components/NavbarComponent.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Link, Outlet, useParams } from 'react-router-dom';
-import { House, BusFront, Sun } from 'react-bootstrap-icons';
-import styles from './NavbarComponent.module.css';
+import React, { useContext } from "react";
+import { Link, Outlet, useParams } from "react-router-dom";
+import { House, BusFront, Sun } from "react-bootstrap-icons";
+import styles from "./NavbarComponent.module.css";
+import { AuthContext } from "./AuthContextProvider";
 
-export default function NavbarComponent({
-}) {
+export default function NavbarComponent() {
+  const { isLoggedIn } = useContext(AuthContext);
 
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to="/" className={styles.navbarItems}> <House></House> Home</Link>
-                <Link to="weather" className={styles.navbarItems}> <Sun></Sun> Weather</Link>
-                <Link to="bus" className={styles.navbarItems}> <BusFront></BusFront> Bus</Link>
-            </nav>
-            <Outlet></Outlet>
-        </>
-    );
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        {isLoggedIn ? (
+          <>
+            <Link to="/" className={styles.navbarItems}>
+              {" "}
+              <House></House> Home
+            </Link>
+            <Link to="weather" className={styles.navbarItems}>
+              {" "}
+              <Sun></Sun> Weather
+            </Link>
+            <Link to="bus" className={styles.navbarItems}>
+              {" "}
+              <BusFront></BusFront> Bus
+            </Link>
+          </>
+        ) : (
+          <p>Please log in.</p>
+        )}
+      </nav>
+      <Outlet></Outlet>
+    </>
+  );
 }
